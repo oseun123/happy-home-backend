b4b0322abecab8206e93389959fd35e1645762ac
Seun <seun04@gmail.com>
2025-05-23 12:16:40 +0100

    add: admin info included in login admin

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -33,7 +33,7 @@ class AdminAuthController extends Controller
         // Generate a Sanctum token
         $token = $superAdmin->createToken('SuperAdmin-API-Token')->plainTextToken;
 
-        return ResponseHelper::withSuccess('Login successful', ['token' => $token]);
+        return ResponseHelper::withSuccess('Login successful', ['token' => $token, 'email' => $request->user('superadmins')->email, 'id' => $request->user('superadmins')->id]);
     }
 
     public function logout(Request $request)
