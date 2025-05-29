6a15f0edb2a77cbde67e4691601b0526900db090
Seun <seun04@gmail.com>
2025-05-29 12:04:29 +0100

    updates:seeder updated

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use App\Models\SuperAdmin;
+use App\Models\Superadmin;
 
 class SuperAdminSeeder extends Seeder
 {
