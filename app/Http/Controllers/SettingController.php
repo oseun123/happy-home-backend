153ba818728d185291da2dd049dbc8f8fa9665a6
Seun <seun04@gmail.com>
2025-06-18 13:07:37 +0100

    update: relationship_type validateion before delete

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -43,20 +43,66 @@ class SettingController extends Controller
     }
 
     // Update an existing setting
+    // public function update(Request $request, $key)
+    // {
+    //     $setting = Setting::where('key', $key)->first();
+
+    //     if (!$setting) {
+    //         return ResponseHelper::withError('Setting not found.');
+    //     }
+
+    //     $request->validate(['value' => 'required|array']);
+
+    //     $setting->update(['value' => $request->value]);
+
+    //     return ResponseHelper::withSuccess('Setting updated successfully.', $setting);
+    // }
+
     public function update(Request $request, $key)
-    {
-        $setting = Setting::where('key', $key)->first();
+{
+    $setting = Setting::where('key', $key)->first();
 
-        if (!$setting) {
-            return ResponseHelper::withError('Setting not found.');
-        }
-
-        $request->validate(['value' => 'required|array']);
-
-        $setting->update(['value' => $request->value]);
-
-        return ResponseHelper::withSuccess('Setting updated successfully.', $setting);
+    if (!$setting) {
+        return ResponseHelper::withError('Setting not found.');
     }
+
+    $request->validate([
+        'value' => 'required|array'
+    ]);
+
+    $incomingValues = $request->value;
+    $currentValues = is_array($setting->value) ? $setting->value : json_decode($setting->value, true);
+    $currentValues = $currentValues ?: [];
+
+    // If the key is "relationship_type", perform special validation
+    if ($key === 'relationship_type') {
+
+        // Get items being *removed* from the current setting
+        $removedValues = array_diff($currentValues, $incomingValues);
+
+        if (!empty($removedValues)) {
+            // Check if any user_preferred_matches is still using the removed values
+            $conflictExists = \DB::table('user_preferred_matches')
+                ->where(function ($query) use ($removedValues) {
+                    foreach ($removedValues as $value) {
+                        $query->orWhereJsonContains('relationship_type', $value);
+                    }
+                })
+                ->exists();
+
+            if ($conflictExists) {
+                return ResponseHelper::withError('Cannot update. One or more relationship types are in use by user preferences.');
+            }
+        }
+    }
+
+    $setting->update([
+        'value' => $incomingValues
+    ]);
+
+    return ResponseHelper::withSuccess('Setting updated successfully.', $setting);
+}
+
 
     // Delete a setting
     public function destroy($key)
